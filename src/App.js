cc4dd38b6ae2917b8832fedce33ff50083ba18dd
olehsm <ohsmyl@gmail.com>
2020-01-29 17:35:35 +0100

    init test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import Amplify, { Predictions} from 'aws-amplify';
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable( new AmazonAIPredictionsProvider() );
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [response, setResponse] =  useState("Translation Output")
+	const [sourceText, setSourceText] = useState("input som text");
+
+	const translate = async () => {
+		const data = await Predictions.convert( {
+			translateText: {
+				source: {text: sourceText}}
+		})
+		setResponse(data.text);
+	}
+
+	const setText = (e) => {
+		setSourceText(e.target.value);
+	}
+
+	return (
+      <div align = "center" style={{backgroundColor: "lightblue"}}>
+        <div>
+          <p>Text Translation</p>
+          <input value={sourceText} onChange={setText}></input>
+          <button onClick={translate}>Click Here to Translate</button>
+          <p>{response}</p>
+        </div>
+      </div>
+    );
 }
+
 
 export default App;
